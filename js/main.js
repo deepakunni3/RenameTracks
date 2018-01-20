468a1263287b055a4bf5dfbff73aa83a814177b4
Deepak Unni <deepak.unni3@gmail.com>
2018-01-19 21:43:06 -0600

    Add plugin

---
+++ js/main.js
@@ -0,0 +1,34 @@
+define([
+           'dojo/_base/declare',
+           'dojo/query',
+           'JBrowse/Plugin'
+       ],
+       function(
+           declare,
+           query,
+           JBrowsePlugin
+       ) {
+return declare( JBrowsePlugin,
+{
+    constructor: function( args ) {
+        console.log("RenameTracks plugin starting");
+        var browser = args.browser;
+
+        dojo.subscribe("/jbrowse/v1/c/tracks/show", function(data) {
+            // everytime a track is shown, check for an alternateKey; if it exists then
+            // replace track's key with the alternateKey
+            for (var i = 0; i < data.length; i++) {
+                var track = data[i];
+                var label = track.label;
+                var alternateKeyName = browser.config.trackMetadata.alternateKey ? browser.config.trackMetadata.alternateKey : "alternate_key";
+                if (alternateKeyName) {
+                    var trackMetadata = browser.trackMetaDataStore.getItem(label);
+                    var alternateKeyValue = trackMetadata[alternateKeyName];
+                    if (alternateKeyValue) browser.trackConfigsByName[label].key = alternateKeyValue;
+                }
+            }
+        });
+
+    }
+});
+});
